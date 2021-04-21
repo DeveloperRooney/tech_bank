4ce9bc798d6c27c36c1e913c213b8189c8ddcace
DeveloperRooney <han_0427@naver.com>
2021-04-21 10:25:03 +0900

    create VO

---
+++ src/main/java/com/sh/techbank/domain/BoardVO.java
@@ -0,0 +1,13 @@
+package com.sh.techbank.domain;
+
+public class BoardVO {
+
+    String subject;
+    String term;
+    String loc;
+    int monthLv;
+    String skill;
+    String category;
+    int consultCnt;
+    String etc;
+}
