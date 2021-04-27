a2573c77e42d60c548ce1218668cd34bfab0b0d1
DeveloperRooney <han_0427@naver.com>
2021-04-27 22:45:26 +0900

    controller modified

--- src/main/java/com/sh/techbank/controller/AdminController.java
+++ src/main/java/com/sh/techbank/controller/AdminController.java
@@ -1,7 +1,14 @@
 package com.sh.techbank.controller;
 
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class AdminController {
@@ -13,12 +20,20 @@ public class AdminController {
         return "";
     }
 
+    @GetMapping("/login.do")
     public String adminLoginForm() {
 
-        return "";
+        return "login";
     }
 
-    public String adminLogin() {
+
+    @PostMapping("/login.pro")
+    public String adminLogin(String userid, String passwd, HttpSession session, RedirectAttributes redirectAttributes){
+
+        System.out.println(userid);
+        System.out.println(passwd);
+
+        session.setAttribute("admin", "admin");
 
         return "";
     }
